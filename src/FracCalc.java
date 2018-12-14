669e66d270a217f333028499d2bd3488746d0a4a
s-baiy <s-baiy@49S50013251.it.bsd405.org>
2018-12-13 19:39:46 -0800

    can't figure out 0

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -19,6 +19,7 @@ public class FracCalc {
     		System.out.println(produceAnswer(userResponse));
     		System.out.print("Enter a fraction equation or type quit to quit");
     		userResponse = s.nextLine();
+    		
     	}
     }
     /**
@@ -35,6 +36,7 @@ public class FracCalc {
     	Scanner s =new Scanner(input);
     	String operand1 = s.next();// will just read to space or end of section
     	String operator = s.next();// + or - or*
+    	System.out.println("operator is " + operator);
     	String operand2= s.next();
     
     	
@@ -45,77 +47,138 @@ public class FracCalc {
     	int newop2Whole=Integer.parseInt(op2Whole);
 
     	
-    	String op2Num = findNum(operand2); // find numerator
+    	String op2Num = findNum(operand2); // find numerator of user input
     	int newop2Num=Integer.parseInt(op2Num);
     	
     	String op2Denom = findDenom(operand2);// find denominator
-    	int newop2Denom=Integer.parseInt(op2Num);
+    	int newop2Denom=Integer.parseInt(op2Denom);
     	
-    	String chk2Answer = "whole:" + op2Whole + " numerator:" + op2Num + " denominator:" + op2Denom;
-    	String op1Whole = findWhole(operand1);
+    	String chk2Answer = "2ND OPERAND whole:" + newop2Whole + " numerator:" + newop2Num + " denominator:" + newop2Denom;
+    	String op1Whole = findWhole(operand1); //separate user input into whole, num and denom
     	int newop1Whole=Integer.parseInt(op1Whole);
+    	//System.out.println(chk2Answer);
     	
     	String op1Num=findNum(operand1);
     	int newop1Num=Integer.parseInt(op1Num);
     	
     	String op1Denom= findDenom(operand1);
     	int newop1Denom=Integer.parseInt(op1Denom);
-    	String op1Answer = "whole:" + op1Whole +" numberator:" + op1Num + " denominator:" + op1Denom;
+    	String op1Answer = "1ST OPERAND whole:" + newop1Whole +" numberator:" + newop1Num + " denominator:" + newop1Denom;
     	
+    	//System.out.println(op1Answer);
     			
     	
         // Checkpoint 3: Evaluate the formula and return the result as a fraction.
         //               Example "4/5 * 1_2/4" returns "6/5".
         //               Note: Answer does not need to be reduced, but it must be correct.
-    	int ImproperNum1 = (newop1Whole* newop1Denom)+newop1Num;
-    	int ImproperDenom1= (newop1Denom);
-    	int ImproperNum2=(newop2Whole*newop2Denom)+newop2Num;
-    	int ImproperDenom2= (newop2Denom);
+    
+    	int impropernum1 = ((Math.abs(newop1Whole) * newop1Denom)+Math.abs(newop1Num));//changes first fraction to improper fraction
+    	if (newop1Whole <0 || newop1Num <0)
+    	{
+    		impropernum1 *= -1;
+    	}
+    	//System.out.println(impropernum1 + "this is impropernum1");// tests impropernum1
+    	
+    	int impropernum2 = ((Math.abs(newop2Whole) * newop2Denom)+ Math.abs(newop2Num));// changes second fraction to improper fraction
+    	if (newop2Whole <0 || newop2Num <0)
+    	{
+    		impropernum2 *= -1;
+    	}
+    	//System.out.println(impropernum2 + "this is impropernum1");// tests impropernum2
+    	int sclimpropernum1 = impropernum1*newop2Denom; //scales the numerator of the first fraction so it matches the denominator
+    	//System.out.println("this is scaled num1: " + sclimpropernum1);
+    	int sclimpropernum2= impropernum2*newop1Denom; //scales the denominator of the second fraction so it matches the denominator
+    	//System.out.println("this is scaled num2: " + sclimpropernum2);
+    	int improperdenom=(newop1Denom*newop2Denom); // scales denominator so they're the same 
+    	
+    	//System.out.println("this is scaled improper frac1: " + sclimpropernum1 + "/" + improperdenom);
+    	//System.out.println("this is scaled improper frac2:" + sclimpropernum2 + "/"+ improperdenom);
+    	String answer= "";
     	int answernum=0;
     	int answerdenom=0;
-    	if (operator == "+")
+    	
+    	if (operator.equals("+")) //adds the two scaled improper fractions
     	{
-    		ImproperNum1 *= ImproperDenom2;
-    		ImproperDenom1 *= ImproperDenom2;
-    		ImproperNum2 *= ImproperDenom1;
-    		ImproperDenom2 *= ImproperDenom1;
-    		answernum=ImproperNum1+ImproperNum2; //add the top and bottom
-    		answerdenom= ImproperDenom2; //both should be the same
+    		answernum = sclimpropernum1 + sclimpropernum2;
     		
+    		answerdenom= improperdenom;
+
+    		answer = answernum + "/" + answerdenom;
+    		//System.out.println(answer);
     	}
-    	else if (operator == "-")
+    	if (operator.equals("-"))
     	{
-    		ImproperNum1 *= ImproperDenom2;
-    		ImproperDenom1 *= ImproperDenom2;
-    		ImproperNum2 *= ImproperDenom1;
-    		ImproperDenom2 *= ImproperDenom1;
-    		answernum=ImproperNum1-ImproperNum2; //subtract top and bottom 
-    		answerdenom= ImproperDenom2; //both should be the same
+    		answernum = sclimpropernum1 - sclimpropernum2;
+    		Integer.toString(answernum);
+    		answerdenom = improperdenom;
+    		Integer.toString(answerdenom);
+    		answer=answernum + "/" + answerdenom;	
+    	}
+    	if (operator.equals("*"))
+    	{
+    		answernum = sclimpropernum1 * sclimpropernum2;
+    		Integer.toString(answernum);
+    		answerdenom = improperdenom * improperdenom;
+    		Integer.toString(answerdenom);
+    		answer = answernum + "/" + answerdenom;
+    	}
+    	if (operator.equals("/"))
+    	{
+    		answernum = sclimpropernum1 * improperdenom;
+    		Integer.toString(answernum);
+    		answerdenom = improperdenom * sclimpropernum2;
+    		answer = answernum+ "/" + answerdenom;
+    	}
+    	System.out.println("here's the improper answer: " + answer);
+    	int gcd = greatestCommonDivisor(answernum, answerdenom);
+    	System.out.println("this is the greatest common divisor: " + gcd);
+    	int reducnum = 0;
+    	int reducdenom=0;
+    	int reducwhole =0;
+    	int reducansnum = 0;
+    	if (Math.abs(answernum)>answerdenom)
+    	{
+    		System.out.println("entered line 141");
+    		reducnum = answernum/gcd;
+    		reducdenom= answerdenom/gcd;
+    		System.out.println("here's the reduced fraction"+ reducnum + "/" + reducdenom);
     		
-    	}
-    	else if (operator == "*")
-    	{
-    		answernum=ImproperNum1*ImproperNum2;
-    		answerdenom = ImproperDenom1 * ImproperDenom2;
-    	}
-    	else if (operator == "/")
-    	{
-    		int flipimpnum2num= ImproperDenom2;
-    		int flipimpnum2denom=ImproperNum2;
-    		answernum=ImproperNum1*flipimpnum2num;
-    		answerdenom = ImproperDenom1 * flipimpnum2denom;
-    	}
-    	return (answernum + "/" + answerdenom);
+    		reducwhole = reducnum/reducdenom;
+   			reducansnum = reducnum % reducdenom;
+   			if (reducnum% reducdenom ==0)
+   			{
+   				answer=Integer.toString(reducwhole);
+   			}
+   			else if (Math.abs(reducwhole)>0)
+   	   			{
+   	   				answer = reducwhole + "_" + reducansnum + "/" + reducdenom;
+   	   			}
+   	    		else
+   	    		{
+   	   				answer = reducansnum + "/" + reducdenom;
+   	   			}
+   		}
+   				
+   	return answer;		
+   	}
+    
+    		
+    
+    	
     
         // Final project: All answers must be reduced.
         //               Example "4/5 * 1_2/4" returns "1_1/5".
         
    
-    }
+    
     // if mixed number, there'll be an underscore
     // TODO: Fill in the space below with helper methods
     
-    			
+    public static int findimpropernum(int newop1Whole, int newop1Num, int newop1Denom )
+    {
+    	int impnum=((newop1Whole * newop1Denom) + newop1Num);
+    	return impnum;
+    }
     
     public static String findWhole (String str)//operand 2 can be mixed, fraction or whole. We're finding the whole number part here.
     {
